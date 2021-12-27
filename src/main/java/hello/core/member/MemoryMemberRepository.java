6fee0b1826d41b0ab12016778cce5c7e7b102b58
hikarigin99 <hikarigin99@gmail.com>
2021-12-27 16:40:05 +0900

    Member 도메인 생성

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();   // 동시성 이슈 발생할 수 있기 때문에 HashMap보다 ConcurrentHashMap 사용해야함
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
